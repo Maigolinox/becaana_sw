eee3cb756fd8e4765769ff16d205f25a581a3ac2
Victor Terron <vicmictm@hotmail.com>
2024-06-07 23:09:45 -0600

    new
    new host

--- becaanaPVm1/settings.py
+++ becaanaPVm1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=oylhm$275w)q8ep!oo7(exy0zvhaxp6$4&k+o^!r@@!e_hast
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.68.193.151','127.0.0.1','10.13.4.38','https://becaana.store']
+ALLOWED_HOSTS = ['54.68.193.151','127.0.0.1','10.13.4.38','https://becaana.store','becaana.store']
 
 ##NECESARIO PARA ALLAUTH
 
